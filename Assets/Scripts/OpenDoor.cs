2c5434d087e9addd3e94db2a31fcd6e8b8c50707
MinjiLee-gloria <gloria.minji.lee@gmail.com>
2020-11-18 19:24:43 +0900

    the first Final version of open door

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private bool doorOpened;
+    private bool coroutineAllowed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpened = false;
+        coroutineAllowed = true;
+        PushButton.ButtonPressed += RunCoroutine;
+    }
+
+    private void RunCoroutine()
+    {
+        StartCoroutine("OpenThatDoor");
+    }
+
+    private IEnumerator OpenThatDoor()
+     {
+        coroutineAllowed = false;
+        if(!doorOpened)
+        {
+            for(float i=0f; i<100f; i += 10f)
+            {
+                transform.rotation = Quaternion.Euler(-90f, 0f, i);
+                yield return new WaitForSeconds(0f);
+            }
+            doorOpened = true;
+        }
+        else
+        {
+            for (float i = 100f; i >= 0f; i -= 10f )
+            {
+                transform.rotation = Quaternion.Euler(-90f, 0f, i);
+                yield return new WaitForSeconds(0f);
+            }
+            doorOpened = false;
+        }
+        coroutineAllowed = true;
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        PushButton.ButtonPressed -= RunCoroutine;
+    }
+}
---
+++ Assets/Scripts/PushButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PushButton : MonoBehaviour
+{
+    public static event Action ButtonPressed = delegate { };
+    private bool coroutineAllowed = true;
+
+    private void OnMouseDown()
+    {
+        if (coroutineAllowed)
+        {
+            StartCoroutine("PushTheButton");
+        }
+    }
+
+    private IEnumerator PushTheButton()
+    {
+        ButtonPressed();
+        coroutineAllowed = false;
+        for (int i = 0; i <= 20; i++)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f);
+            yield return null;
+        }
+        for (int i = 0; i <= 20; i++)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.1f);
+            yield return null;
+        }
+        coroutineAllowed = true;
+    }
+
+}
